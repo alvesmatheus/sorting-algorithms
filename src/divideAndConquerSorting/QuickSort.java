e6f85430604d6c2b60063823f3675529f6327396
SpinnelSun <m.hollygard@gmail.com>
2018-05-18 22:50:39 -0300

    Adição do Merge e Quick

---
+++ src/divideAndConquerSorting/QuickSort.java
@@ -0,0 +1,129 @@
+package divideAndConquerSorting;
+
+import abstractSorting.AbstractSorting;
+import utility.Utility;
+
+/**
+ * Quicksort is based on the divide-and-conquer paradigm. The algorithm chooses
+ * a pivot element and rearranges the elements of the interval in such a way
+ * that all elements lesser than the pivot go to the left part of the array and
+ * all elements greater than the pivot, go to the right part of the array. Then
+ * it recursively sorts the left and the right parts.
+ * 
+ * - COMPLEXITY: O(n2)
+ * - IN-PLACE: Yes
+ * - STABLE: No
+ * 
+ * @author Matheus Alves dos Santos
+ * 
+ */
+public class QuickSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+	/**
+	 * This method sorts an interval of the array, starting at leftIndex and ending
+	 * at rightIndex, both inclusive. The array must not contain null elements. If
+	 * leftIndex and/or rightIndex are outside the array limits, this method will do
+	 * absolutely nothing. It implements the quick sort strategy.
+	 * 
+	 * @param array
+	 *            The target array of the sorting algorithm.
+	 * @param leftIndex
+	 *            The index where the sorting should begin.
+	 * @param rightIndex
+	 *            The index where the sorting should end.
+	 * 
+	 */
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		// Only arrays that go through all validations will be sorted.
+		if (this.inputValidation(array, leftIndex, rightIndex)) {
+			int pivotIndex = partition(array, leftIndex, rightIndex);
+
+			sort(array, leftIndex, pivotIndex - 1);
+			sort(array, pivotIndex + 1, rightIndex);
+		}
+	}
+
+	/**
+	 * This method validates all the parameters received by the sort method. The
+	 * array received must not be null or contain less than two elements. Also, the
+	 * index parameters given may be suitable to the array.
+	 * 
+	 * @param array
+	 *            The target array of the sorting algorithm.
+	 * @param leftIndex
+	 *            The index where the sorting should begin.
+	 * @param rightIndex
+	 *            The index where the sorting should end.
+	 * 
+	 * @return the boolean that represents the prameter's validity.
+	 * 
+	 */
+	private boolean inputValidation(T[] array, int leftIndex, int rightIndex) {
+		boolean isValid = true;
+
+		// A null can't be sorted.
+		if (array == null) {
+			isValid = false;
+		}
+
+		// Arrays containing less than two elements don't need to be sorted.
+		else if (array.length < 2) {
+			isValid = false;
+		}
+
+		else {
+			// Prevent misuse of indexes parameters.
+			if (leftIndex > rightIndex) {
+				isValid = false;
+			}
+
+			// Prevent attempts to access invalid indexes.
+			if (leftIndex < 0) {
+				isValid = false;
+			}
+
+			if (rightIndex < 0) {
+				isValid = false;
+			}
+
+			if (rightIndex > (array.length - 1)) {
+				isValid = false;
+			}
+		}
+
+		return isValid;
+	}
+
+	/**
+	 * After choosing a pivot element, the one at the leftIndex, this method will
+	 * reorder the array so that all elements with values less or equal than the
+	 * pivot come before it, while all elements with values greater than the pivot
+	 * come after it. After this partitioning, the pivot is in its final position.
+	 * 
+	 * @param array
+	 *            The target array of the sorting algorithm.
+	 * @param leftIndex
+	 *            The index where the sorting should begin.
+	 * @param rightIndex
+	 *            The index where the sorting should end.
+	 * 
+	 * @return the pivot's final position.
+	 * 
+	 */
+	public int partition(T[] array, int leftIndex, int rightIndex) {
+		T pivot = array[leftIndex];
+
+		int i = leftIndex;
+
+		for (int j = i + 1; j <= rightIndex; j++) {
+			if (array[j].compareTo(pivot) <= 0) {
+				i++;
+				Utility.swap(array, i, j);
+			}
+		}
+
+		Utility.swap(array, leftIndex, i);
+		return i;
+	}
+}
